1aa0d3032875a3862e8dab108add49ab4a9905cd
lukasbonthy <126826970+lukasbonthy@users.noreply.github.com>
2023-05-11 21:41:04 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,26 @@
+javascript
 importScripts('./uv/uv.sw.js');
 
-const sw = new UVServiceWorker();
+class CustomServiceWorker {
+  constructor() {
+    this.cacheName = 'uv-static';
+  }
 
-self.addEventListener('fetch', event =>
-    event.respondWith(
-        sw.fetch(event)
-    )
-);
+  async fetch(event) {
+    const cachedResponse = await caches.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    }
+
+    const networkResponse = await fetch(event.request);
+    const cache = await caches.open(this.cacheName);
+    cache.put(event.request, networkResponse.clone());
+    return networkResponse;
+  }
+}
+
+const sw = new CustomServiceWorker();
+
+self.addEventListener('fetch', event => {
+  event.respondWith(sw.fetch(event));
+});
